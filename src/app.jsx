2fffd95a33d2457cd06b78d4e364439fb683a928
Vitalii <vittiger09@gmail.com>
2021-10-05 14:47:57 +0300

    DZ Routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import NavBar from "./components/navBar";
 import Users from "./components/users";
+import { Route, Switch } from "react-router-dom";
+import MainPage from "./components/mainPage";
+import Login from "./components/login";
 
 const App = () => {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact component={MainPage} />
+                <Route path="/login" component={Login} />
+                <Route path="/users" component={Users} />
+            </Switch>
+        </>
+    );
 };
 
 export default App;
