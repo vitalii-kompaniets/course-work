f58f8682056101ae94ef13850ec0dab37441c6f7
Vitalii <vittiger09@gmail.com>
2021-10-16 15:13:14 +0300

    Login Form

--- src/layouts/users.jsx
+++ src/layouts/users.jsx
@@ -6,7 +6,7 @@ import UsersList from "../components/usersList";
 const Users = () => {
     const params = useParams();
     const { userId } = params;
-    return <>{userId ? <UserPage /> : <UsersList />}</>;
+    return <>{userId ? <UserPage userId={userId} /> : <UsersList />}</>;
 };
 
 export default Users;
